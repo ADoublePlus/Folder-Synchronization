5f914ab96963611c7014793b66858edbd80f1eca
John Wonson <jmpwonson@gmail.com>
2021-01-08 14:16:38 +1100

    Create FileActions.cs

---
+++ FolderSyncCSharp/FolderSyncCSharp/FileActions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Folder_Synchronization
+{
+    public class FileActions
+    {
+        private String sourceFolder;
+        private String targetFolder;
+
+        public FileActions(Configuration configuration)
+        {
+            sourceFolder = configuration.SourcePath;
+            targetFolder = configuration.TargetPath;
+        }
+
+        public void changed(String sourcePath)
+        {
+            String targetPath = getTargetPath(sourcePath, targetFolder);
+
+            if (File.Exists(targetPath))
+            {
+                File.Delete(targetPath);
+            }
+
+            File.Copy(sourcePath, targetPath);
+        }
+
+        public void created(String sourcePath)
+        {
+            changed(sourcePath);
+        }
+
+        public void deleted(String sourcePath)
+        {
+            String targetPath = getTargetPath(sourcePath, targetFolder);
+
+            if (File.Exists(targetPath))
+            {
+                File.Delete(targetPath);
+            }
+        }
+
+        public void renamed(String oldPath, String oldName, String newPath)
+        {
+            FileInfo source = new FileInfo(newPath);
+            String fileToFind = targetFolder + "\\" + oldName;
+            String fileTarget = targetFolder + "\\" + source.Name;
+
+            if (File.Exists(fileToFind))
+            {
+                File.Move(fileToFind, fileTarget);
+            }
+        }
+
+        private String getTargetPath(String sourcePath, String targetFolder)
+        {
+            FileInfo source = new FileInfo(sourcePath);
+            return targetFolder + "\\" + source.Name;
+        }
+    }
+}
