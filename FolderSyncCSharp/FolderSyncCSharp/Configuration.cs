7ec51d0ca9714dc104f359000f9a996caf16043a
John Wonson <jmpwonson@gmail.com>
2021-01-08 14:09:27 +1100

    Create Configuration.cs

---
+++ FolderSyncCSharp/FolderSyncCSharp/Configuration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Folder_Synchronization
+{
+    public class Configuration
+    {
+        private const String RegistryHome = @"Software\ADoublePlus\Folder-Synchronization";
+
+        private String sourcePath;
+
+        public String SourcePath
+        {
+            get { return sourcePath; }
+        }
+
+        private String targetPath;
+
+        public String TargetPath
+        {
+            get { return targetPath; }
+        }
+
+        private ILogger logger;
+
+        public Configuration(ILogger logger)
+        {
+            this.logger = logger;
+
+            loadFromRegistry();
+        }
+
+        private void loadFromRegistry()
+        {
+            try
+            {
+                using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+                using (var sourceKey = hklm.OpenSubKey(RegistryHome))
+                {
+                    if (sourceKey != null)
+                    {
+                        Object source = sourceKey.GetValue("SourcePath");
+
+                        if (source != null)
+                        {
+                            sourcePath = source as String;
+                        }
+
+                        Object target = sourceKey.GetValue("TargetPath");
+
+                        if (target != null)
+                        {
+                            targetPath = target as String;
+                        }
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                logger.LogToError(e.Message);
+            }
+        }
+    }
+}
