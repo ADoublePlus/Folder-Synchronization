5115b69d43889ee5d41427d1db42d0934e614b96
John Wonson <jmpwonson@gmail.com>
2021-01-08 15:33:15 +1100

    Create ProjectInstaller.cs & Designer.cs variant

---
+++ FolderSyncCSharp/FolderSyncCSharp/ProjectInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace Folder_Synchronization
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void serviceProcessInstaller_AfterInstall(object sender, InstallEventArgs e) { }
+    }
+}
