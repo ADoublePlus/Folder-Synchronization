8f5014954ace7a63b53e47709ada186f10aabac2
John Wonson <jmpwonson@gmail.com>
2021-01-08 14:41:15 +1100

    Create WindowsEventLog.cs

---
+++ FolderSyncCSharp/FolderSyncCSharp/WindowsEventLog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Folder_Synchronization
+{
+    class WindowsEventLog : ILogger
+    {
+        private String applicationName;
+
+        public WindowsEventLog(String applicationName)
+        {
+            this.applicationName = applicationName;
+        }
+
+        public void LogToError(string message)
+        {
+            using (EventLog eventLog = new EventLog(applicationName))
+            {
+                eventLog.Source = applicationName;
+                eventLog.WriteEntry(message, EventLogEntryType.Error);
+            }
+        }
+
+        public void LogToInfo(string message)
+        {
+            using (EventLog eventLog = new EventLog(applicationName))
+            {
+                eventLog.Source = applicationName;
+                eventLog.WriteEntry(message, EventLogEntryType.Information);
+            }
+        }
+
+        public void LogToWarning(string message)
+        {
+            using (EventLog eventLog = new EventLog(applicationName))
+            {
+                eventLog.Source = applicationName;
+                eventLog.WriteEntry(message, EventLogEntryType.Warning);
+            }
+        }
+    }
+}
